7fca24ec3e672ee75faac27c81339540dcf90c47
boekenbox <pim.graus@gmail.com>
2020-10-27 16:32:32 +0100

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b38552023ccfecfbffc4bbeea9c9acd.js"
+  "/precache-manifest.391bee11f673eedb176d2bba9f1295ab.js"
 );
 
 self.addEventListener('message', (event) => {
